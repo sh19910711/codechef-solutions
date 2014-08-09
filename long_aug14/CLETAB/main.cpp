d6d8c5704e23501295a420556be2f1094aaa48d6
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-09 19:37:17 +0900

    CLETAB: Wrong Answer

--- long_aug14/CLETAB/main.cpp
+++ long_aug14/CLETAB/main.cpp
@@ -12,7 +12,7 @@ typedef long long Int;
 const Int N_MAX = 200 + 11;
 const Int M_MAX = 400 + 11;
 const Int C_MAX = 400 + 11;
-const Int MEMO_NONE = -1;
+const Int INF = ( 10000 + 11 );
 
 Int N;
 Int M;
@@ -26,31 +26,58 @@ void input() {
   for ( int i = 0; i < M; ++ i ) C[i] --;
 }
 
-Int cnt[M_MAX];
-Int ml[C_MAX];
-Int mr[C_MAX];
+typedef tuple <Int, Int> Node; // next, c_id
+typedef priority_queue <Node, vector<Node>, greater<Node>> Queue;
+
+Int O[C_MAX][2 * M_MAX];
+Int OC[C_MAX];
+Int OI[C_MAX];
+bool D[C_MAX];
 
 void init() {
-  fill(cnt, cnt + M_MAX, 0);
-  fill(ml, ml + C_MAX, -1);
-  fill(mr, mr + C_MAX, -1);
+  for ( int i = 0; i < C_MAX; ++ i ) {
+    fill(O[i], O[i] + 2 * M_MAX, INF);
+  }
+  fill(OC, OC + C_MAX, 0);
+  fill(OI, OI + C_MAX, 0);
+  fill(D, D + C_MAX, false);
 }
 
 Int solve() {
   for ( int i = 0; i < M; ++ i ) {
     const auto& c_id = C[i];
-    if ( ml[c_id] == -1 ) ml[c_id] = i;
-    mr[c_id] = i;
+    auto& n = OC[c_id];
+    O[c_id][n ++] = i;
   }
-  for ( int i = 0; i < C_MAX; ++ i ) {
-    const auto& c_id = i;
-    if ( ml[c_id] != -1 ) {
-      for ( int j = ml[c_id]; j <= mr[c_id]; ++ j ) {
-        cnt[j] ++;
+
+  Queue Q;
+  Int res = 0;
+  for ( int i = 0; i < M; ++ i ) {
+    const auto& c_id = C[i];
+    if ( D[c_id] ) {
+      Queue q;
+      while ( ! Q.empty() ) {
+        const auto& x = Q.top();
+        Q.pop();
+        if ( c_id != get<1>(x) ) q.push(x);
       }
+      Q.swap(q);
+      Q.emplace(O[c_id][OI[c_id] ++], c_id);
+      continue;
     }
+    if ( Q.size() < N ) {
+      Q.emplace(O[c_id][OI[c_id] ++], c_id);
+    } else {
+      while ( ! D[get<1>(Q.top())] ) Q.pop();
+      const auto& rm = Q.top();
+      const auto& rm_c_id = get<1>(rm);
+      D[rm_c_id] = false;
+      Q.emplace(O[c_id][OI[c_id] ++] - i, c_id);
+    }
+    res ++;
+    D[c_id] = true;
   }
-  return -1;
+  return res;
 }
 
 int main() {
