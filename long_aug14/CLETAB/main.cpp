e6b2fd72cf8fc7654208e4fb12b777b86682a2a7
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-09 19:52:21 +0900

    CLETAB: Wrong Answer

--- long_aug14/CLETAB/main.cpp
+++ long_aug14/CLETAB/main.cpp
@@ -27,7 +27,7 @@ void input() {
 }
 
 typedef tuple <Int, Int> Node; // next, c_id
-typedef priority_queue <Node, vector<Node>, greater<Node>> Queue;
+typedef priority_queue <Node, vector<Node>> Queue;
 
 Int O[C_MAX][2 * M_MAX];
 Int OC[C_MAX];
@@ -39,8 +39,19 @@ void init() {
     fill(O[i], O[i] + 2 * M_MAX, INF);
   }
   fill(OC, OC + C_MAX, 0);
-  fill(OI, OI + C_MAX, 0);
+  fill(OI, OI + C_MAX, 1);
   fill(D, D + C_MAX, false);
+}
+
+void print_node( const Node& node ) {
+  cout << get<0>(node) << ", " << get<1>(node) << endl;
+}
+
+void print_queue( Queue Q ) {
+  while ( ! Q.empty() ) {
+    print_node(Q.top());
+    Q.pop();
+  }
 }
 
 Int solve() {
@@ -53,16 +64,24 @@ Int solve() {
   Queue Q;
   Int res = 0;
   for ( int i = 0; i < M; ++ i ) {
+    // cout << "i = " << i << endl; print_queue(Q); cout << endl;
+
     const auto& c_id = C[i];
+
+    Queue q;
+    while ( ! Q.empty() ) {
+      auto x = Q.top();
+      Q.pop();
+      if ( get<0>(x) != INF ) get<0>(x) --;
+      if ( c_id != get<1>(x) ) q.push(x);
+    }
+    Q.swap(q);
+
     if ( D[c_id] ) {
-      Queue q;
-      while ( ! Q.empty() ) {
-        const auto& x = Q.top();
-        Q.pop();
-        if ( c_id != get<1>(x) ) q.push(x);
-      }
-      Q.swap(q);
-      Q.emplace(O[c_id][OI[c_id] ++], c_id);
+      // cout << "keep: " << c_id << endl;
+      auto t = O[c_id][OI[c_id] ++];
+      if ( t != INF ) t -= i;
+      Q.emplace(t, c_id);
       continue;
     }
     if ( Q.size() < N ) {
@@ -71,11 +90,15 @@ Int solve() {
       while ( ! D[get<1>(Q.top())] ) Q.pop();
       const auto& rm = Q.top();
       const auto& rm_c_id = get<1>(rm);
+      // cout << "remove: " << rm_c_id << endl;
       D[rm_c_id] = false;
-      Q.emplace(O[c_id][OI[c_id] ++] - i, c_id);
+      auto t = O[c_id][OI[c_id] ++];
+      if ( t != INF ) t -= i;
+      Q.emplace(t, c_id);
     }
     res ++;
     D[c_id] = true;
+    // cout << "add: " << c_id << endl;
   }
   return res;
 }
