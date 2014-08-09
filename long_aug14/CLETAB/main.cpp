7f804dc2fad527184b939794fff06d6a9a7ba6cd
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-09 18:41:58 +0900

    CLETAB: wip

--- long_aug14/CLETAB/main.cpp
+++ long_aug14/CLETAB/main.cpp
@@ -26,61 +26,31 @@ void input() {
   for ( int i = 0; i < M; ++ i ) C[i] --;
 }
 
-Int orders[C_MAX][M_MAX]; // orders[c_id][i] := c_id's i-th order pos in C
-Int order_num[C_MAX];
-Int pos_to_order[C_MAX][M_MAX];
-bool keeped[M_MAX];
-Int memo[M_MAX][M_MAX];
+Int cnt[M_MAX];
+Int ml[C_MAX];
+Int mr[C_MAX];
 
 void init() {
-  for ( int i = 0; i < M_MAX; ++ i ) {
-    fill(memo[i], memo[i] + M_MAX, MEMO_NONE);
-  }
-  fill(order_num, order_num + C_MAX, 0);
-  fill(keeped, keeped + M_MAX, false);
-}
-
-Int rec( const Int& l, const Int& r ) {
-  auto& res = memo[l][r];
-  if ( res != MEMO_NONE ) return res;
-  res = 0;
-  bool flag = false;
-  for ( Int i = l + 1; i < r; ++ i ) {
-    const auto& c_id = C[i];
-    const auto& order_ind = pos_to_order[c_id][i];
-    if ( order_ind + 1 < order_num[c_id] ) {
-      flag = true;
-      const auto& ret = rec(i, min(r, orders[c_id][order_ind + 1]));
-      res += ret;
-    }
-  }
-  if ( ! flag ) return 1;
-  return res;
+  fill(cnt, cnt + M_MAX, 0);
+  fill(ml, ml + C_MAX, -1);
+  fill(mr, mr + C_MAX, -1);
 }
 
 Int solve() {
   for ( int i = 0; i < M; ++ i ) {
     const auto& c_id = C[i];
-    orders[c_id][order_num[c_id]] = i;
-    pos_to_order[c_id][i] = order_num[c_id];
-    order_num[c_id] ++;
+    if ( ml[c_id] == -1 ) ml[c_id] = i;
+    mr[c_id] = i;
   }
-  Int res = 0;
-  for ( int i = 0; i < M; ++ i ) {
-    const auto& c_id = C[i];
-    const auto& order_ind = pos_to_order[c_id][i];
-    if ( order_ind + 1 < order_num[c_id] ) {
-      const auto& next_order_pos = orders[c_id][order_ind + 1];
-      const auto& ret = rec(i, next_order_pos);
-      if ( ret < N ) {
-        if ( ! keeped[i] ) res ++;
-        keeped[next_order_pos] = true;
-        continue;
+  for ( int i = 0; i < C_MAX; ++ i ) {
+    const auto& c_id = i;
+    if ( ml[c_id] != -1 ) {
+      for ( int j = ml[c_id]; j <= mr[c_id]; ++ j ) {
+        cnt[j] ++;
       }
     }
-    if ( ! keeped[i] ) res ++;
   }
-  return res;
+  return -1;
 }
 
 int main() {
